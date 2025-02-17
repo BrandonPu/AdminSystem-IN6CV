618718f961efc29fabc3eecc38ea1850882d69a1
BrandonPu <bpu-2023395@Kinal.edu.gt>
2025-02-17 09:19:56 -0600

    se agrego el eliminar

--- src/users/user.routes.js
+++ src/users/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { check } from "express-validator";
-import { getUsers } from "./user.controller.js";
+import { getUsers, deleteUser } from "./user.controller.js";
 import { existeUsuarioById } from "../helpers/db-validator.js";
 import { validarCampos } from "../middlewares/validar-campos.js";
 import { tieneRole } from "../middlewares/validar-role.js";
@@ -10,5 +10,14 @@ const router = Router();
 
 router.get("/", getUsers);
 
+router.delete(
+    "/:id",
+    [
+        check("id", "No es un ID valido").isMongoId(),
+        check("id").custom(existeUsuarioById),
+        validarCampos
+    ],
+    deleteUser
+)
 
 export default router;
